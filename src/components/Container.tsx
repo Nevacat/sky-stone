c4fda191d128eaba970a44f4ed98d8c247ae2627
Nevacat <nevacattery217@naver.com>
2024-02-05 16:52:31 +0900

    stlyes: Container padding제거

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,10 +10,6 @@ const Container = ({children}:ContainerProps) => {
     <div className='
       max-w-[2520px]
       mx-auto
-      xl:px-20
-      md:px-10
-      sm:px-2
-      px-4
       py-6
     '> 
       {children}
