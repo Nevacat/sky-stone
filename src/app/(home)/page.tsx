e58fc7f3bd795d5e12d57760c6ae084f9e48bae7
Nevacat <nevacattery217@naver.com>
2024-02-05 16:05:30 +0900

    feat: 메인페이지 파일 경로 재설정 및 컨테이너 생성

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Container>
+      home
+    </Container>
+  )
+}
+
+export default Home
