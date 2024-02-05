e6bf902c26e01c3f93511705a2d0c9acc0d53c8c
Nevacat <nevacattery217@naver.com>
2024-02-05 16:54:17 +0900

    feat: imageSlide컴포넌트 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,11 @@
 import Container from '@/components/Container'
+import ImageSlide from '@/components/Home/ImageSlide'
 import React from 'react'
 
 const Home = () => {
   return (
     <Container>
-      home
+      <ImageSlide/>
     </Container>
   )
 }
